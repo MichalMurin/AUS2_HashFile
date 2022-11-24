a9a25cd49256d89b71da69977417269c686f49ee
MichalMurin <murin.michal.945@gmail.com>
2022-11-25 00:03:54 +0100

    implemented trie

---
+++ AUS2_MichalMurin_HashFile/DataStructures/Trie/ExternNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AUS2_MichalMurin_HashFile.DataStructures.Trie
+{
+    internal class ExternNode: TrieNode
+    {
+        public int RecordsCount { get; set; }
+        public long Offset { get; set; }
+
+        public ExternNode(long pOffset, int pRecordsCount, TrieNode? parent = null) : base(parent)
+        {
+            IsExternNode = true;
+            Offset = pOffset;
+            RecordsCount = pRecordsCount;
+        }
+    }
+}
---
+++ AUS2_MichalMurin_HashFile/DataStructures/Trie/TrieNode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AUS2_MichalMurin_HashFile.DataStructures.Trie
+{
+    internal class TrieNode
+    {
+        /// <summary>
+        /// Rodic vrcholu, pre Root je rodic = null
+        /// </summary>
+        public TrieNode? Parent { get; set; }
+        //public bool IsExternNode { get; protected set; }
+
+        /// <summary>
+        /// Konstruktor, ktory vytvori vrchol a vlozi do neho data
+        /// </summary>
+        public TrieNode(TrieNode? parent = null)
+        {
+            Parent = parent;
+        }
+    }
+}
